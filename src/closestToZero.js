3bc7544adf20912abd303d9ca3550f003f7ae47c
Leila El Ghrib <leila.elghrib@gmail.com>
2020-05-06 18:00:59 +0200

    implement closestToZero function

--- src/closestToZero.js
+++ src/closestToZero.js
@@ -1,3 +1,16 @@
-const closestToZero = (input) => {};
+const closestToZero = (input) => {
+  if (input === undefined || input.length === 0) return 0;
+
+  let closest;
+  input.forEach((number) => {
+    if (
+      closest === undefined ||
+      Math.abs(closest) > Math.abs(number) ||
+      (Math.abs(closest) === Math.abs(number) && closest < number)
+    )
+      closest = number;
+  });
+  return closest;
+};
 
 export default closestToZero;
